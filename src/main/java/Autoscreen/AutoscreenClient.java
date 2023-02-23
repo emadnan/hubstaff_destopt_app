a3fd9d7bd0d6f4f0b2f7a78fe0aa07b717f3c604
jahanzaibkhan1 <jahanzaibkorai75@gmail.com>
2023-02-23 17:14:11 +0500

    initial commit

---
+++ src/main/java/Autoscreen/AutoscreenClient.java
@@ -0,0 +1,10 @@
+package Autoscreen;
+
+public class AutoscreenClient {
+    public AutoscreenClient(String s, String skTest26PHem9AhJZvU623DfE1x4sd) {
+    }
+
+    public String uploadScreenshot(byte[] imageData) {
+        return null;
+    }
+}
